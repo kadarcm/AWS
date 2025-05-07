1745419e556aec044fd5d08e94f32894b950edd5
Chris Kadar <kadarcm@gmail.com>
2025-05-06 22:51:15 -0400

    fix bug

--- .github/build.sh
+++ .github/build.sh
@@ -5,9 +5,8 @@ cd react_site
 npm install
 npm run build
 echo "build complete"
-echo "copying build home_site
-cd ..
 echo "copying build home_site"
+cd ..
 cp -r react_site/build/* ./home_site/react_site
 echo "deploying to s3"
 aws s3 sync ./home_site/react_site s3://kadarcm.com --delete
